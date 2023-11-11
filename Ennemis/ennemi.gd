8bd6b3a67623cbbe1db9a6d775a1be24352a9360
Tianhong258 <huangtianhong258@gmail.com>
2023-11-11 19:02:08 +0100

    Ajouter l'ennemie avec un point de vie 20

---
+++ Ennemis/ennemi.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+@export var speed = 40.0
+@export var health = 20.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var direction =  Vector2(-1.0, -0.0)
+	velocity = direction*speed
+	move_and_slide()
+	
+func health_control(x):
+	health -= x
+	if health <= 0.0:
+		queue_free()
+	
